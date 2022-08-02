d49f57f31533356289eb22898dc7000e0a5e9f78
BaljitSingh13 <109940253+BaljitSingh13@users.noreply.github.com>
2022-08-02 11:46:33 +0530

    Assignment Second

---
+++ AssignmentSecond.java
@@ -0,0 +1,183 @@
+package o7_taining;
+
+import java.util.Scanner;
+
+
+public class AssignmentSecond {
+                                       // 1st ANSWER
+	/*
+	public static void main(String[] args) {
+	System.out.print("enter the number:");
+	Scanner obj= new Scanner(System.in);
+	int a=obj.nextInt();
+
+	if(a%2==0)
+	{
+		System.out.println(a+" is an even");
+	}
+	else
+	{System.out.println(a+" is an odd");}
+	}
+*/
+	
+	
+	                                     //2nd ANSWER
+
+	/*
+	public static void main(String[] args) {
+		System.out.print("enter the number:");
+		Scanner obj= new Scanner(System.in);
+		int a=obj.nextInt();
+	
+		if(a>55)
+		{ System.out.println(a+" is greater than 55");}
+		
+		else
+		{System.out.println(a+" is smaller than 55");}
+	}
+
+*/
+	
+	
+	                                      //3rd ANSWER
+/*	
+	public static void main(String[] args) {
+		System.out.print("enter the number:");
+		Scanner obj= new Scanner(System.in);
+		int a=obj.nextInt();
+		int b=obj.nextInt();
+		
+		if(a==b)
+		{System.out.println("both the numbers are equal");}
+		
+		else
+		{System.out.println("both the number are not equals");}
+		
+		
+			if(a>b)
+			{System.out.println(a+"is greater than "+b);}
+			
+			else
+			{System.out.println(b+" is greater than "+a);}
+				
+		}
+	
+	*/	
+	
+	                                  // 4th ANSWER
+
+	/*
+	public static void main(String[] args) {
+		System.out.println("enter the number: ");
+		Scanner obj= new Scanner(System.in);
+		int a=obj.nextInt();
+	
+		switch(a) 
+		{
+		case 579:
+		{System.out.println("Sono's Rollno");}
+		break;
+		
+		case 546:
+		{System.out.println("Monu's Rollno");}
+		break;
+		
+		case 553:
+		{System.out.println("Manu's Rollno");}
+		break;
+			
+		case 544:
+		{System.out.println("Meenu's Rollno");}
+		break;
+		
+		case 516:
+		{System.out.println("Meena's Rollno");}
+		break;
+		
+		default:
+		{System.out.println("Show message Not Allowed");}
+		
+	}
+  }
+	
+	*/
+	
+	
+                                  	// 6th ANSWER
+	
+	/*
+	public static void main(String[] args) {
+		System.out.print("enter the number:");
+		Scanner obj= new Scanner(System.in);
+		int a=obj.nextInt();
+		
+		for(int i=1;i<=a;i++)
+		{System.out.println(i);}
+	
+}
+
+   */
+	
+	                                //7th ANSWER
+	
+/*
+	
+	 public static void main(String[] args) {
+	 
+		System.out.print("enter the size of the array:");
+		Scanner obj= new Scanner(System.in);
+		int a=obj.nextInt();
+	
+		int b[]=new b [11];
+		
+		for(int i=0;i<=11;i++)
+		{  int c []= obj.nextInt[10];}
+		{System.out.println();}
+		
+		{System.out.println("enter the elements of the array");}
+		for(int i=0;i<=10;i++)
+		{System.out.println("elements are:"+c[i]);}
+		
+		
+	
+	 }
+
+	 */
+	
+	
+	                                   //8th QUESTION
+	 public static void main(String[] args) {
+	    System.out.print("enter the number:");
+	    Scanner obj= new Scanner(System.in);
+		int a=obj.nextInt(); 
+			
+			for(int i=0;i<3;i++)
+				
+			{
+				for(int j=0;j<4;j++)
+					
+				{
+					if(i%2==0)
+					{System.out.println("10101");}
+					else
+					{System.out.println("01010");}
+				}
+			}
+	 }
+}
+
+
+                                    // 5th ANSWER
+/*
+PASS BY VALUE-> java only support pass by value.it is the state when anyone made any change in a file
+                then that change reflect back in the original file also i.e if we made any single change in
+                a file then changed file(information) will be appered on  the another end or to the new person
+                who is viewing that file
+                
+PASS BY REFERENCE-> java does not support pass by reference.It is the state when anyone made any change in a file
+                then that change does not reflect back to the original file i.e original file remain unchanged although
+                changes are made a file then changed file(information) will be appered on  the another end or to the
+                new person who is viewing that file  
+                example:if we change the original value which is 23 to 34 then it does not reflect back or no any change 
+                is made in the original value.
+	*/
